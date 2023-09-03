0727dcd232e52cc8d6ddc6fbb58ae6f00ca92295
LeGeek01 <timeobailly02@gmail.com>
2023-09-03 20:13:11 +0200

    edit service in login pronote

--- views/AuthStack/Pronote/LoginPronote.js
+++ views/AuthStack/Pronote/LoginPronote.js
@@ -156,7 +156,7 @@ function LoginPronote({ route, navigation }) {
       else {
         AsyncStorage.setItem('token', token);
         AsyncStorage.setItem('credentials', JSON.stringify(credentials));
-        AsyncStorage.setItem('service', "pronote");
+        AsyncStorage.setItem('service', "Pronote");
 
         showMessage({
           message: "Connecté avec succès",
