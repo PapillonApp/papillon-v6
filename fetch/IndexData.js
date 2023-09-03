5a5a460031cd40dc957d49052ffd6327ea1505f2
LeGeek01 <timeobailly02@gmail.com>
2023-09-03 21:06:01 +0200

    fix indexdata

--- fetch/IndexData.js
+++ fetch/IndexData.js
@@ -1,10 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-let service = AsyncStorage.getItem("service")
+let service;
+AsyncStorage.getItem("service").then((value) => {
+    service = value
+})
 
 //[Service]Grades.js
 
-export function getGrades(force = false) {
-    if(service === "Pronote") return require(`./PronoteData/PronoteGrades.js`).getGrades(force)
+export async function getGrades(force = false) {
+    console.log("Service : " + service)
+    if(service === "Pronote") {
+        let result = await require(`./PronoteData/PronoteGrades.js`).getGrades(force)
+        console.log("Résultat : " + result)
+        return result;
+    }
 }
 
 export function getEvaluations(force = false) {
