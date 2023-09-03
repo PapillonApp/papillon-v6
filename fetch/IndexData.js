3e2d08ed30cc73850fd42c347a0cd99a6d58bd2a
LeGeek01 <timeobailly02@gmail.com>
2023-09-03 21:20:13 +0200

    finition indexdata

--- fetch/IndexData.js
+++ fetch/IndexData.js
@@ -17,3 +17,37 @@ export function getEvaluations(force = false) {
 export function changePeriod(period) {
     if(service === "Pronote") return require(`./PronoteData/PronoteGrades.js`).changePeriod(period)
 }
+
+//[Service]Homeworks.js
+export function getHomeworks(day) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteHomeworks.js`).getHomeworks(day)
+}
+
+export function changeHomeworkState(day, id) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteHomeworks.js`).changeHomeworkState(day, id)
+}
+
+//[Service]News.js
+export function getNews(force = false) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteNews.js`).getNews(force)
+}
+
+//[Service]Recap.js
+export function getRecap(day, force = false) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteRecap.js`).getRecap(day, force = false)
+}
+
+//[Service]Timetable.js
+export function getTimetable(day, force = false) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteTimetable.js`).getTimetable(day, force)
+}
+
+//[Service]User.js
+export function getUser(force = false) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteUser.js`).getUser(force)
+}
+
+//[Service]Viesco.js
+export function getViesco(force = false) {
+    if(service === "Pronote") return require(`./PronoteData/PronoteViesco.js`).getViesco(force)
+}
