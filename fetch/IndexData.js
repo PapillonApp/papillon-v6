b33b0028d561bcf7fb35b19886b7452872535c37
Vince Linise <vincelinise@gmail.com>
2023-09-10 18:34:09 +0200

    Fix du fix du pire bug de l'univers

--- fetch/IndexData.js
+++ fetch/IndexData.js
@@ -5,21 +5,21 @@ import asyncStorage from '@react-native-async-storage/async-storage';
 export class IndexData {
   // [Service]Grades.js
   static async getGrades(force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteGrades.js`).getGrades(force);
     });
   }
 
   static async getEvaluations(force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteGrades.js`).getEvaluations(force);
     });
   }
 
   static async changePeriod(period) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteGrades.js`).changePeriod(period);
     });
@@ -27,14 +27,14 @@ export class IndexData {
 
   // [Service]Homeworks.js
   static async getHomeworks(day) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteHomeworks.js`).getHomeworks(day);
     });
   }
 
   static async changeHomeworkState(day, id) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteHomeworks.js`).changeHomeworkState(
           day,
@@ -45,7 +45,7 @@ export class IndexData {
 
   // [Service]News.js
   static async getNews(force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteNews.js`).getNews(force);
     });
@@ -53,7 +53,7 @@ export class IndexData {
 
   // [Service]Recap.js
   static async getRecap(day, force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteRecap.js`).getRecap(day, force);
     });
@@ -61,7 +61,7 @@ export class IndexData {
 
   // [Service]Timetable.js
   static async getTimetable(day, force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteTimetable.js`).getTimetable(
           day,
@@ -72,7 +72,7 @@ export class IndexData {
 
   // [Service]User.js
   static async getUser(force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteUser.js`).getUser(force);
     });
@@ -80,7 +80,7 @@ export class IndexData {
 
   // [Service]Viesco.js
   static async getViesco(force = false) {
-    asyncStorage.getItem('service').then((service) => {
+    return asyncStorage.getItem('service').then((service) => {
       if (service === 'Pronote')
         return require(`./PronoteData/PronoteViesco.js`).getViesco(force);
     });
