3b6cf5645635f99dfb28c004fd217236f184d209
LeGeek01 <timeobailly02@gmail.com>
2023-09-28 08:34:38 +0200

    fix bug moyenne classe

--- views/GradesScreen.js
+++ views/GradesScreen.js
@@ -192,7 +192,8 @@ function GradesScreen({ navigation }) {
       }
 
       // if class_overall_average exists in grades
-      if (gradesData.class_overall_average && gradesData.class_overall_average !== null) {
+      if (gradesData.class_overall_average && gradesData.class_overall_average !== null && gradesData.class_overall_average === "-1") {
+        console.log("moyenne classe dans data", gradesData.class_overall_average)
         classAverage = gradesData.class_overall_average;
         console.log('classAverage', classAverage);
       }
