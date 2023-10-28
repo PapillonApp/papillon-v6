97804a9c39fb23f0e482fe34f76891b21f32fcae
LeGeek01 <timeobailly02@gmail.com>
2023-10-28 12:30:49 +0200

    add widget registration index

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+
+import { registerWidgetTaskHandler } from 'react-native-android-widget';
+
+import { widgetTaskHandler } from './android-widgets/widget-task-handler';
+
+registerWidgetTaskHandler(widgetTaskHandler);
