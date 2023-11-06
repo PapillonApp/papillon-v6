f6811c8a04d9873e9b35cfae926b6528f02989dd
LeMaitre4523 <79224590+LeMaitre4523@users.noreply.github.com>
2023-11-06 22:36:49 +0100

    Edit ED_init

--- fetch/EcoleDirecteData/ED_init.js
+++ fetch/EcoleDirecteData/ED_init.js
@@ -2,13 +2,16 @@ import ED from 'papillon-ed-core';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default async function() {
-    const [token, user_id] = await Promise.all([
+    const [token, user, school, modules] = await Promise.all([
         AsyncStorage.getItem("token"),
-        AsyncStorage.getItem("ED_ID").then(
-            (_studentId) => JSON.parse(_studentId)
-        ),
+        AsyncStorage.getItem("ED_USERDATA").then((userData) => JSON.parse(userData)),
+        AsyncStorage.getItem("ED_SCHOOLDATA").then((schoolData) => JSON.parse(schoolData)),
+        AsyncStorage.getItem("ED_MODULES").then((modules) => JSON.parse(modules)),
     ]);
     let edInstance = new ED();
-    edInstance.auth.setToken(token, user_id);
+    edInstance.auth.setToken(token, user.id);
+    edInstance.student = user;
+    edInstance.school = school;
+    edInstance.modules = modules;
     return edInstance;
 }
