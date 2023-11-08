94f1b9244531e643167fd223ada0f2485b705e04
Vince Linise <vincelinise@gmail.com>
2023-11-08 20:16:06 +0100

    Fix news notification

--- fetch/BackgroundFetch.js
+++ fetch/BackgroundFetch.js
@@ -18,7 +18,7 @@ TaskManager.defineTask('background-fetch-news', async () => {
         if (news.length !== oldNews.length) {
           AsyncStorage.setItem('oldNews', JSON.stringify(news));
 
-          const lastNews = news[0];
+          const lastNews = news[news.length - 1];
 
           Notifications.postLocalNotification({
             body: lastNews.title,
