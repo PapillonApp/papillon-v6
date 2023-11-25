93d89057c2d661bbe7181ee19e13a4fa86e9b519
Vince Linise <vincelinise@gmail.com>
2023-11-25 19:20:59 +0100

    Génération des couleurs de cours

--- views/Settings/CoursColor.js
+++ views/Settings/CoursColor.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, Button, StyleSheet, TouchableOpacity, Image, ScrollView, TextInput, Alert, Modal } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { View, Button, StyleSheet, TouchableOpacity, Image, ScrollView, TextInput, Alert, Modal, Pressable } from 'react-native';
 
 import { useTheme, Text } from 'react-native-paper';
 import GetUIColors from '../../utils/GetUIColors'
@@ -20,6 +20,7 @@ import ColorPicker, {
 import formatCoursName from '../../utils/FormatCoursName';
 
 import { forceSavedCourseColor } from '../../utils/ColorCoursName';
+import { Dice5, Lock } from 'lucide-react-native';
 
 const CoursColor = ({ navigation }) => {
   const theme = useTheme();
@@ -30,6 +31,12 @@ const CoursColor = ({ navigation }) => {
   const [colorModalOpen, setColorModalOpen] = useState(false);
   const [colorModalColor, setColorModalColor] = useState('#000000');
   const [currentEditedSubject, setCurrentEditedSubject] = useState('');
+
+  const colors = [
+    '#2667a9', '#76a10b', '#3498DB', '#1ABC9C', '#a01679', '#27AE60', '#156cd6', '#F39C12', '#E67E22', '#D35400', '#2C3E50', '#E74C3C', '#C0392B', '#8E44AD', '#ad4491', '#9f563b', '#920205',
+    '#6a42a3', '#498821', '#2e86b3', '#17a085', '#89125f', '#2f9e49', '#1e6fcf', '#d08e15', '#b85f18', '#a33e00', '#3f515f', '#c92a1e', '#a82b1f', '#7b389f', '#a65089', '#996032', '#8c0101',
+    '#6b064d', '#146c80', '#7c9f18', '#9f5610', '#b23e00', '#34495e', '#a3180f', '#891e13', '#623c85', '#b5657e', '#a6794a', '#b60000',
+  ];
 
   const onSelectColor = ({ hex }) => {
     setColorModalColor(hex);
@@ -50,6 +57,50 @@ const CoursColor = ({ navigation }) => {
     setColorModalOpen(false)
   };
 
+  const ApplyRandomColors = () => {
+    let col = {};
+    let usedColors = [];
+
+    let lockedColors = [];
+
+    const randomColor = () => {
+      let color = colors[Math.floor(Math.random() * colors.length)];
+      
+      if (usedColors.includes(color)) {
+        return randomColor();
+      } else {
+        usedColors.push(color);
+        return color;
+      }
+    };
+
+    Object.keys(savedColors).forEach((key) => {
+      if (savedColors[key].locked) {
+        lockedColors.push(key);
+      }
+      else {
+        let color = randomColor();
+        forceSavedCourseColor(key, color);
+      }
+    });
+
+    let newCol = JSON.parse(SyncStorage.get('savedColors'));
+
+    // remove all entries with no color
+    Object.keys(newCol).forEach((key) => {
+      if (!newCol[key].color) {
+        delete newCol[key];
+      }
+    });
+
+    // add locked  to all entries
+    Object.keys(newCol).forEach((key) => {
+      newCol[key].locked = lockedColors.includes(key);
+    });
+
+    setSavedColors(newCol);
+  };
+
   useEffect(() => {
     SyncStorage.init();
 
@@ -62,8 +113,29 @@ const CoursColor = ({ navigation }) => {
       }
     });
 
+    // add locked : false to all entries
+    Object.keys(col).forEach((key) => {
+      col[key].locked = false;
+    });
+
     setSavedColors(col);
   }, []);
+
+  // add dice button in header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => {
+            ApplyRandomColors();
+          }}
+          style={{ padding: 10, marginRight: -7 }}
+        >
+          <Dice5 color={UIColors.primary} />
+        </TouchableOpacity>
+      ),
+    });
+  });
 
   return (
     <ScrollView
@@ -78,9 +150,24 @@ const CoursColor = ({ navigation }) => {
             <NativeItem
               key={index}
               leading={
-                <View style={{ width: 18, height: 18, borderRadius: 12, backgroundColor: savedColors[key].color }} />
+                <View style={[styles.colorPreview, {backgroundColor: savedColors[key].color }]} />
               }
 
+              trailing={
+                <LockToggle
+                  color={savedColors[key].color}
+                  value={savedColors[key].locked}
+                  onValueChange={(value) => {
+                    setSavedColors({
+                      ...savedColors,
+                      [key]: {
+                        ...savedColors[key],
+                        locked: value,
+                      },
+                    });
+                  }}
+                />
+              }
 
               onPress={() => {
                 setColorModalOpen(true);
@@ -140,6 +227,41 @@ const CoursColor = ({ navigation }) => {
   )
 };
 
+const LockToggle = ({ value, onValueChange, color }) => {
+  const [locked, setLocked] = useState(value);
+  const UIColors = GetUIColors();
+
+  return (
+    <Pressable
+      onPress={() => {
+        setLocked(!locked);
+        onValueChange(!locked);
+      }}
+
+      style={[
+        {
+          backgroundColor: UIColors.text + '15',
+          padding: 7,
+          borderRadius: 100,
+        },
+        locked ? {
+          backgroundColor: UIColors.text,
+        } : {},
+      ]}
+    >
+      <Lock 
+        size={19}
+        color={
+          locked ? UIColors.background : UIColors.text
+        }
+        style={[
+          !locked ? {opacity: 0.4} : {},
+        ]}
+      />
+    </Pressable>
+  );
+};
+
 const styles = StyleSheet.create({
   colorModalContainer: {
     flex: 1,
@@ -173,6 +295,14 @@ const styles = StyleSheet.create({
   modalActionText: {
     fontFamily: 'Papillon-Semibold',
     fontSize: 16,
+  },
+
+  colorPreview: {
+    width: 48,
+    height: 20,
+    borderRadius: 6,
+    borderCurve: 'continuous',
+    marginRight: 2,
   }
 });
 
