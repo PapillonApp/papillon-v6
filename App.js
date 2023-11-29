4a9dbeac4b533c1260f194ab9148798a200627d1
Vince Linise <vincelinise@gmail.com>
2023-11-29 18:31:22 +0100

    Amélioration du nouvel header

--- App.js
+++ App.js
@@ -734,8 +734,8 @@ const styles = StyleSheet.create({
   },
 
   headerSideButton: {
-    width: 40,
-    height: 40,
+    width: 38,
+    height: 38,
     borderRadius: 56,
     justifyContent: 'center',
     alignItems: 'center',
@@ -793,7 +793,7 @@ function Header(props) {
         { props.options.headerLeft ? props.options.headerLeft() : showBack ?
           <TouchableOpacity
             onPress={() => props.navigation.goBack()}
-            style={[styles.headerSideButton, {backgroundColor: UIColors.text + '22'}]}
+            style={[styles.headerSideButton, {backgroundColor: UIColors.text + '18'}]}
           >
             <ChevronLeft size={28} color={UIColors.text + 'e5'} />
           </TouchableOpacity>
