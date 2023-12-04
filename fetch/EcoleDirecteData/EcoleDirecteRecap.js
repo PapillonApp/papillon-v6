d813e26f146311ca11a7245b00344867203ffb28
LeMaitre4523 <79224590+LeMaitre4523@users.noreply.github.com>
2023-12-04 22:05:11 +0100

    Create EcoleDirecte Recap

---
+++ fetch/EcoleDirecteData/EcoleDirecteRecap.js
@@ -0,0 +1,50 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { getTimetable } from './EcoleDirecteTimetable';
+import { getHomeworks } from './EcoleDirecteHomeworks';
+import { getGrades } from './EcoleDirecteGrades';
+
+function addDays(date, days) {
+  date = new Date(date);
+  date.setDate(date.getDate() + days);
+
+  return date;
+}
+
+function getRecap(day, force, ecoledirecteInstance) {
+  return AsyncStorage.getItem('recapCache').then((recapCache) => {
+    if (recapCache && !force) {
+      recapCache = JSON.parse(recapCache);
+
+      const userCacheDate = new Date(recapCache.date);
+      const today = new Date();
+
+      userCacheDate.setHours(0, 0, 0, 0);
+      today.setHours(0, 0, 0, 0);
+
+      if (userCacheDate.getTime() === today.getTime()) return recapCache.recap;
+
+      AsyncStorage.removeItem('recapCache');
+      return getRecap(day, true);
+    }
+    return Promise.all([
+      getTimetable(day, force, ecoledirecteInstance),
+      getGrades(force, ecoledirecteInstance),
+      getHomeworks(day, force, ecoledirecteInstance),
+      /*getHomeworks(addDays(day, 1), force, ecoledirecteInstance),
+      getHomeworks(addDays(day, 2), force, ecoledirecteInstance),*/
+    ]).then((result) => {
+      // send to widget
+      AsyncStorage.setItem(
+        'recapCache',
+        JSON.stringify({
+          date: new Date(),
+          recap: result,
+        })
+      );
+      return result;
+    });
+  });
+}
+
+export { getRecap };
