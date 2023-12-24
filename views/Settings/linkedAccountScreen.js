d0351152e8a119c24a77c21cf99528637fd235c5
Rémy Godet <77058107+godetremy@users.noreply.github.com>
2023-12-24 15:40:18 +0100

    🧐 Fix bug: Res parse failed

--- views/Settings/linkedAccountScreen.js
+++ views/Settings/linkedAccountScreen.js
@@ -1,119 +1,110 @@
 import * as React from 'react';
-import {useEffect} from 'react';
-import {ActivityIndicator, Alert, ScrollView, TouchableOpacity} from 'react-native';
+import { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  Alert,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
+import { Plus, Trash } from 'lucide-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import NativeList from '../../components/NativeList';
 import NativeItem from '../../components/NativeItem';
 import NativeText from '../../components/NativeText';
 
-import {Plus, Trash} from 'lucide-react-native';
 import Services from './services.json';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
-function LinkedAccountScreen({navigation}) {
-	const [accounts, setAccounts] = React.useState([]);
-	const [loading, setLoading] = React.useState(true);
+function LinkedAccountScreen({ navigation }) {
+  const [accounts, setAccounts] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-	function getAccounts() {
-		setLoading(true)
-		AsyncStorage.getItem('linkedAccount').then((result) => {
-			console.log(result)
-			var res = {restaurant:{}}
-			if (result != null) {
-				res = JSON.parse(result)
-			};
-			console.log(res)
-			setAccounts(res);
-			setLoading(false);
-		});
-	}
+  function getAccounts() {
+    setLoading(true);
+    AsyncStorage.getItem('linkedAccount').then((result) => {
+      let res = { restaurant: {} };
+      if (result != null) {
+        res = JSON.parse(result);
+      }
+      setAccounts(res);
+    });
+  }
 
-	function getServicesName(id) {
-		var res = 'Services inconu';
-		Services.restaurant.forEach((service) => {
-			if (service.id === id) {
-				res = service.name;
-			}
-		});
-		return res;
-	}
+  function getServicesName(id) {
+    let res = 'Services inconu';
+    Services.restaurant.forEach((service) => {
+      if (service.id === id) {
+        res = service.name;
+      }
+    });
+    return res;
+  }
 
-	function deleteRestaurant() {
-		Alert.alert(
-			'Supprimer le compte',
-			'Voulez-vous vraiment supprimer ce compte ?',
-			[
-				{
-					text: 'Annuler',
-					onPress: () => {
+  function deleteRestaurant() {
+    Alert.alert(
+      'Supprimer le compte',
+      'Voulez-vous vraiment supprimer ce compte ?',
+      [
+        {
+          text: 'Annuler',
+          onPress: () => {},
+          style: 'cancel',
+        },
+        {
+          text: 'Supprimer',
+          onPress: () => {
+            AsyncStorage.getItem('linkedAccount').then((result) => {
+              const res = JSON.parse(result || { restaurant: {} });
+              res.restaurant = {};
+              setAccounts(res);
+              AsyncStorage.setItem('linkedAccount', JSON.stringify(res));
+            });
+          },
+          style: 'destructive',
+        },
+      ]
+    );
+  }
 
-					},
-					style: 'cancel'
-				},
-				{
-					text: 'Supprimer',
-					onPress: () => {
-						AsyncStorage.getItem('linkedAccount').then((result) => {
-							let res = JSON.parse(result || {restaurant: {}});
-							res.restaurant = {}
-							setAccounts(res);
-							AsyncStorage.setItem('linkedAccount', JSON.stringify(res));
-						});
-					},
-					style: 'destructive'
-				}
-			]
-		)
-	}
+  React.useLayoutEffect(() => {
+    getAccounts();
+    setLoading(false);
+  }, []);
 
-	React.useLayoutEffect(() => {
-		getAccounts();
-		return
-	}, [accounts])
+  return (
+    <ScrollView>
+      {JSON.stringify(accounts.restaurant) === '{}' ? null : (
+        <NativeList inset header="Service de restauration">
+          <NativeItem
+            trailing={
+              <TouchableOpacity onPress={() => deleteRestaurant()}>
+                <Trash color="#2A937A" />
+              </TouchableOpacity>
+            }
+          >
+            <NativeText heading="h4">
+              {getServicesName(accounts.restaurant?.id)}
+            </NativeText>
+            <NativeText heading="p2">
+              {accounts.restaurant?.username}
+            </NativeText>
+          </NativeItem>
+        </NativeList>
+      )}
 
-
-
-	return (
-		<ScrollView>
-			{JSON.stringify(accounts.restaurant) === '{}' ?
-				null
-				: (
-					<NativeList inset header='Service de restauration'>
-						<NativeItem
-							trailing={
-								<TouchableOpacity onPress={() => deleteRestaurant()}>
-									<Trash
-										color="#2A937A"
-									/>
-								</TouchableOpacity>
-							}
-						>
-							<NativeText heading="h4">{getServicesName(accounts.restaurant?.id)}</NativeText>
-							<NativeText heading="p2">{accounts.restaurant?.username}</NativeText>
-						</NativeItem>
-					</NativeList>
-				)
-			}
-
-			<NativeList inset>
-				{loading ?
-					null
-					:
-					(
-						<ActivityIndicator/>
-					)
-				}
-				<NativeItem
-					leading={
-						<Plus size={20} color="#2A937A"/>
-					}
-					onPress={() => navigation.navigate('AddLinkedAccount')}
-				>
-					<NativeText heading="p" style={{color: '#2A937A'}}>Ajouter un compte</NativeText>
-				</NativeItem>
-			</NativeList>
-		</ScrollView>
-	)
+      <NativeList inset>
+        {loading ? <ActivityIndicator /> : null}
+        <NativeItem
+          leading={<Plus size={20} color="#2A937A" />}
+          onPress={() => navigation.navigate('AddLinkedAccount')}
+        >
+          <NativeText heading="p" style={{ color: '#2A937A' }}>
+            Ajouter un compte
+          </NativeText>
+        </NativeItem>
+      </NativeList>
+    </ScrollView>
+  );
 }
 
 export default LinkedAccountScreen;
