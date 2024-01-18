9ccbcfeae85f8e386994c5b9367792203b9d4ecc
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-18 08:36:58 +0100

    feat(hooks): add debouncer

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const useDebounce = <T>(value: T, delay = 500): T => {
+  const [debouncedValue, setDebouncedValue] = React.useState<T>(value);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
