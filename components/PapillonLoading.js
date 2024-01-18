c59dc8f999504f6ba52669164c6861715fc2bc73
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-18 08:36:50 +0100

    fix(PapillonLoading): default value for style

--- components/PapillonLoading.js
+++ components/PapillonLoading.js
@@ -4,7 +4,7 @@ import { Text } from 'react-native-paper';
 
 import GetUIColors from '../utils/GetUIColors';
 
-function PapillonLoading({ title, subtitle, icon, style }) {
+function PapillonLoading({ title, subtitle, icon, style = {} }) {
   const UIColors = GetUIColors();
 
   return (
