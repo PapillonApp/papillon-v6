409a8395c4e54ada56b9de357ee63a114482057a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-18 15:46:40 +0100

    refactor(PronoteLogin): only support student menu and login with pawnote
    - in the future : will be replaced with a webview to login from browser
    - a selector for the type of account (that will redirect to that webview)

--- views/NewAuthStack/Pronote/NGPronoteLogin.tsx
+++ views/NewAuthStack/Pronote/NGPronoteLogin.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React from 'react';
+
 import {
   ScrollView,
   View,
@@ -8,165 +9,75 @@ import {
   StatusBar,
   ActivityIndicator,
   Image,
-  Switch,
-  Dimensions
 } from 'react-native';
 
-import { useActionSheet } from '@expo/react-native-action-sheet';
-
-import LinearGradient from 'react-native-linear-gradient';
-
+import { UserCircle, KeyRound, AlertTriangle } from 'lucide-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import LinearGradient from 'react-native-linear-gradient';
+import { showMessage } from 'react-native-flash-message';
 import { useTheme, Text } from 'react-native-paper';
 
-import { showMessage } from 'react-native-flash-message';
-
-import { useState } from 'react';
-
-import { UserCircle, KeyRound, AlertTriangle } from 'lucide-react-native';
-import { getENTs, getInfo, getToken } from '../../../fetch/AuthStack/LoginFlow';
 
 import PapillonButton from '../../../components/PapillonButton';
 import NativeList from '../../../components/NativeList';
 import NativeItem from '../../../components/NativeItem';
-import NativeText from '../../../components/NativeText';
 
 import AlertBottomSheet from '../../../interface/AlertBottomSheet';
 
 import GetUIColors from '../../../utils/GetUIColors';
 import { useAppContext } from '../../../utils/AppContext';
 
-import SegmentedControl from 'react-native-segmented-control-2';
+import { getPronoteInstanceInformation, defaultPawnoteFetcher, authenticatePronoteCredentials, PronoteApiAccountId } from 'pawnote';
+import { AsyncStoragePronoteKeys } from '../../../fetch/PronoteData/connector';
+
+type PronoteInstanceInformation = Awaited<ReturnType<typeof getPronoteInstanceInformation>>
 
 function NGPronoteLogin({ route, navigation }) {
+  const instanceURL: string = route.params.instanceURL;
+  
   const theme = useTheme();
-  const { showActionSheetWithOptions } = useActionSheet();
 
-  const { etab, useDemo } = route.params as {
-    etab: { nomEtab: string, url: string }
-    useDemo?: boolean
-  };
-
-  const [etabName, setEtabName] = useState(etab.nomEtab);
-  const [etabInfo, setEtabInfo] = useState(etab);
-
-  const [useEduconnect, setUseEduconnect] = React.useState(false);
-
-  const [errorAlert, setErrorAlert] = useState(false);
-
-  const [isENTUsed, setIsENTUsed] = React.useState(false);
-  const onToggleSwitch = () => setIsENTUsed(!isENTUsed);
-
-  const onToggleParent = () => setModeParent(!modeParent);
-
-  let CAS = null;
-  const [ENTs, setENTs] = useState([]);
-
-  const [modeParent, setModeParent] = useState(false);
-
-  function removeSubdomain(hostname) {
-    if (!hostname) return;
-
-    let domain = hostname.split('.');
-
-    if (domain.length > 2) {
-      // remove everything except the last two parts
-      domain = domain.slice(domain.length - 2, domain.length);
-
-      return domain.join('.');
-    }
-
-    return hostname;
-  }
+  const [errorAlert, setErrorAlert] = React.useState(false);
+  const [instanceDetails, setInstanceDetails] = React.useState<PronoteInstanceInformation | null>(null);
 
   React.useEffect(() => {
-    getENTs(etab.url).then((result) => {
-      CAS = result.CAS;
-      navigation.setOptions({});
+    (async () => {
+      try {
+        const details = await getPronoteInstanceInformation(defaultPawnoteFetcher, {
+          pronoteURL: instanceURL
+        });
+  
+        setInstanceDetails(details);
+      }
+      catch {
+        setInstanceDetails(null);
+        setErrorAlert(true);
+      }
+    })();
+  }, [instanceURL]);
 
-      setEtabInfo(result);
-      setEtabName(result.nomEtab);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [connecting, setConnecting] = React.useState(false);
 
-      const hostname = CAS.casURL.split('/')[2];
+  const appContext = useAppContext();
+  const UIColors = GetUIColors();
 
-      getInfo().then((r) => {
-        const ents = r?.ent_list;
+  const makeUUID = (): string => {
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      (c) => {
+        const r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  };
 
-        // find all ent in ents where url = hostname
-        const entList = ents.filter(
-          (ent) => removeSubdomain(ent.url) === removeSubdomain(hostname)
-        );
-
-        let ent = entList[0];
-
-        if (entList.length > 1) {
-          showActionSheetWithOptions(
-            {
-              title: 'Choisissez votre ENT',
-              options: entList.map((_ent) => _ent.name),
-              cancelButtonIndex: entList.length,
-              tintColor: UIColors.primary,
-            },
-            (buttonIndex) => {
-              if (buttonIndex < entList.length) {
-                ent = entList[buttonIndex];
-                setENTs(ent);
-
-                if (ent?.educonnect) {
-                  setUseEduconnect(ent.educonnect);
-                } else {
-                  setUseEduconnect(false);
-                }
-
-                if (ent) {
-                  setIsENTUsed(true);
-                }
-              }
-            }
-          );
-        }
-
-        setENTs(ent);
-
-        if (ent?.educonnect) {
-          setUseEduconnect(ent.educonnect);
-        } else {
-          setUseEduconnect(false);
-        }
-
-        if (ent) {
-          setIsENTUsed(true);
-        }
-      });
-    });
-  }, []);
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [connecting, setConnecting] = useState(false);
-
-  const appctx = useAppContext();
-
-  function login() {
-    let finalURL = etab.url.toLowerCase();
-
-    const credentials = {
-      username,
-      password,
-      url: finalURL,
-      ent: isENTUsed ? ENTs.py : '',
-      parent: modeParent,
-    };
-
-    if (!isENTUsed && ENTs !== undefined && ENTs.py !== undefined) {
-      credentials.url += '?login=true';
-    }
-
-    if (
-      credentials.username.trim() === '' ||
-      credentials.password.trim() === ''
-    ) {
+  const handleLogin = async () => {
+    if (username.trim() === '' || password.trim() === '') {
       showMessage({
         message: 'Échec de la connexion',
         description: 'Veuillez remplir tous les champs.',
@@ -175,47 +86,60 @@ function NGPronoteLogin({ route, navigation }) {
         floating: true,
         duration: 5000,
       });
+
       return;
     }
-    setConnecting(true);
-    getToken(credentials).then((result) => {
+
+    try {
+      setConnecting(true);
+  
+      const pronoteURL = instanceDetails.pronoteRootURL;
+      const deviceUUID = makeUUID();
+  
+      const pronote = await authenticatePronoteCredentials(pronoteURL, {
+        username, password,
+        accountTypeID: PronoteApiAccountId.Eleve,
+        deviceUUID,
+      });
+  
+      await AsyncStorage.multiSet([
+        [AsyncStoragePronoteKeys.NEXT_TIME_TOKEN, pronote.nextTimeToken],
+        [AsyncStoragePronoteKeys.ACCOUNT_TYPE_ID, pronote.accountTypeID.toString()],
+        [AsyncStoragePronoteKeys.INSTANCE_URL, pronote.pronoteRootURL],
+        [AsyncStoragePronoteKeys.USERNAME, pronote.username],
+        [AsyncStoragePronoteKeys.DEVICE_UUID, deviceUUID],
+      ]);
+  
       setConnecting(false);
-      const token = result.token;
-
-      if (!token) {
-        setErrorAlert(true);
-      } else {
-        AsyncStorage.setItem('token', token);
-        AsyncStorage.setItem('credentials', JSON.stringify(credentials));
-        AsyncStorage.setItem('service', 'Pronote');
-
-        showMessage({
-          message: 'Connecté avec succès',
-          type: 'success',
-          icon: 'auto',
-        });
-
-        appctx.dataprovider.service = 'Pronote';
-        appctx.dataprovider.init('Pronote').then(() => {
-          navigation.goBack();
-          navigation.goBack();
-          appctx.setLoggedIn(true);
-        });
-      }
-    });
-  }
-
-  React.useEffect(() => {
-    if (useDemo) {
-      setUsername('demonstration');
-      setPassword('pronotevs');
+  
+      showMessage({
+        message: 'Connecté avec succès',
+        type: 'success',
+        icon: 'auto',
+      });
+  
+      await appContext.dataProvider.init('pronote', pronote);
+      
+      navigation.goBack();
+      navigation.goBack();
+      appContext.setLoggedIn(true);
     }
-  }, []);
+    catch {
+      setConnecting(false);
 
-  const UIColors = GetUIColors();
+      showMessage({
+        message: 'Échec de la connexion',
+        description: 'Une erreur s\'est produite.',
+        type: 'danger',
+        icon: 'auto',
+        floating: true,
+        duration: 5000,
+      });
+    }
+  };
+
 
   return (
-    
     <>
       <LinearGradient
         colors={[UIColors.modalBackground, UIColors.modalBackground + '00']}
@@ -229,10 +153,7 @@ function NGPronoteLogin({ route, navigation }) {
           zIndex: 9999,
         }}
       />
-      <ScrollView
-        style={[styles.container, { backgroundColor: UIColors.modalBackground }]}
-      >
-
+      <ScrollView style={{ backgroundColor: UIColors.modalBackground }}>
         <AlertBottomSheet
           title="Échec de la connexion"
           subtitle="Vérifiez vos identifiants et réessayez."
@@ -263,72 +184,13 @@ function NGPronoteLogin({ route, navigation }) {
             source={require('../../../assets/logo_pronote.png')}
           />
           <Text style={styles.loginHeaderText}>
-            {entities.decodeHTML(etab.nomEtab)}
+            {instanceDetails.schoolName}
           </Text>
 
-          {!isENTUsed ? (
-            <Text style={styles.loginHeaderDescription}>
+          <Text style={styles.loginHeaderDescription}>
             Identifiants PRONOTE
-            </Text>
-          ) : (
-            <Text style={styles.loginHeaderDescription}>
-              {ENTs.name}
-            </Text>
-          )}
+          </Text>
         </View>
-
-
-        { Platform.OS === 'ios' ? (
-          <SegmentedControl
-            tabs={['Espace élèves', 'Espace parents']}
-            width={
-              Dimensions.get('window').width > 600 ? 600 : undefined
-            }
-            style={{ 
-              backgroundColor: UIColors.text + '12',
-              marginHorizontal: 15,
-              alignSelf: 'center',
-            }}
-            activeTabColor={
-              theme.dark ? '#333333' :
-                UIColors.element
-            }
-            activeTextColor={UIColors.text}
-            textStyle={{ color: UIColors.text + '55' }}
-            onChange={(index) => {
-              setModeParent(index === 1);
-            }}
-          />
-        ) : (
-          <SegmentedControl
-            tabs={['Espace élèves', 'Espace parents']}
-            style={{ 
-              backgroundColor: UIColors.text + '00',
-              marginHorizontal: 15,
-              borderRadius: 0,
-              height: 42,
-            }}
-            tabStyle={{
-              borderRadius: 0,
-            }}
-            selectedTabStyle={{
-              borderRadius: 0,
-              elevation: 0,
-              borderBottomColor: UIColors.primary,
-              borderBottomWidth: 2,
-              borderTopLeftRadius: 4,
-              borderTopRightRadius: 4,
-            }}
-            activeTabColor={
-              UIColors.primary + '00'
-            }
-            activeTextColor={UIColors.primary}
-            textStyle={{ color: UIColors.text + '55', marginTop: -2, fontSize:16, fontFamily: 'Papillon-Semibold' }}
-            onChange={(index) => {
-              setModeParent(index === 1);
-            }}
-          />
-        )}
 
         {Platform.OS === 'android' ? (
           <View style={{ height: 15 }} />
@@ -368,31 +230,16 @@ function NGPronoteLogin({ route, navigation }) {
               onChangeText={(text) => setPassword(text)}
             />
           </NativeItem>
-
-          {ENTs !== undefined && ENTs.length !== 0 ? (
-            <NativeItem
-              trailing={
-                <Switch value={isENTUsed} onValueChange={onToggleSwitch} color={UIColors.primary} />
-              }
-            >
-              <View style={{paddingVertical: 4, gap:2}}>
-                <NativeText heading="p2" style={{fontSize: 15}}>
-                Se connecter avec l'ENT
-                </NativeText>
-                <NativeText heading="h4">
-                  {ENTs.name}
-                </NativeText>
-              </View>
-            </NativeItem>
-          ) : <View/>}
         </NativeList>
 
         <View style={[styles.loginForm, Platform.OS !== 'ios' && styles.loginFormAndroid]}>
           <View style={[styles.buttons]}>
             <PapillonButton
+              left={null}
+              light={null}
               title="Se connecter"
               color="#159C5E"
-              onPress={() => login()}
+              onPress={() => handleLogin()}
               style={[styles.button]}
               right={connecting && <ActivityIndicator color="#ffffff" />}
             />
@@ -402,25 +249,6 @@ function NGPronoteLogin({ route, navigation }) {
             <View style={{ height: 15 }} />
           ) : null}
 
-          { modeParent ? (
-            <NativeList inset>
-              <NativeItem
-                leading={
-                  <AlertTriangle
-                    color={'#FFC107'}
-                  />
-                }
-              >
-                <NativeText heading="h4">
-              Support des comptes parents en beta
-                </NativeText>
-                <NativeText heading="p2">
-              Il est possible que certaines fonctionnalités ne soient pas disponibles ou ne fonctionnent pas correctement.
-                </NativeText>
-              </NativeItem>
-            </NativeList>
-          ) : null }
-
           <View style={[styles.bottomText]}>
             <Text style={[styles.bottomTextText]}>
             En vous connectant, vous acceptez les{' '}
@@ -428,49 +256,13 @@ function NGPronoteLogin({ route, navigation }) {
             de Papillon.
             </Text>
 
-            {etabInfo.version && etabInfo.version.length > 0 ? (
-              <Text style={[styles.bottomTextText]}>
-              Pronote Espace {!modeParent ? 'Élèves' : 'Parents'} ver. {etabInfo.version.join('.')}
-              </Text>
-            ) : null}
+            <Text style={[styles.bottomTextText]}>
+              Pronote Espace Élèves version {instanceDetails.version}
+            </Text>
           </View>
         </View>
       </ScrollView>
     </>
-  );
-}
-
-function LoginTextInput({
-  label,
-  icon,
-  value,
-  onChangeText,
-  secureTextEntry,
-  style,
-}) {
-  const theme = useTheme();
-
-  return (
-    <View
-      style={[
-        styles.loginTextInput,
-        { borderColor: theme.dark ? '#191919' : '#e5e5e5' },
-        style,
-      ]}
-    >
-      {icon}
-      <TextInput
-        style={[
-          styles.loginTextInputText,
-          { color: theme.dark ? '#fff' : '#000' },
-        ]}
-        placeholder={label}
-        value={value}
-        onChangeText={onChangeText}
-        secureTextEntry={secureTextEntry}
-        placeholderTextColor={theme.dark ? '#ffffff55' : '#00000055'}
-      />
-    </View>
   );
 }
 
