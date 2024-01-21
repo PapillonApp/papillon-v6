b5525df13fabd1fb12a6ab05ecfec6f3ff762ce2
Mikkel RINGAUD <mikkel@milescode.dev>
2024-01-21 18:50:42 +0100

    refactor(PapillonIcon): typescript

---
+++ components/PapillonIcon.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, StyleSheet, type ViewStyle } from 'react-native';
+
+interface Props {
+  icon: React.ReactElement
+  /** Should be in HEX format. */
+  color: string
+  /** Whether the icon should be filled or not. */
+  fill?: boolean
+  /** Removes the background of the icon. */
+  plain?: boolean
+  /** Show icon in a smaller size. */
+  small?: boolean
+  style?: ViewStyle
+}
+
+const PapillonIcon: React.FC<Props> = ({ icon, color, style, fill, small, plain }) => {
+  let opacity = '10';
+  if (fill) opacity = 'FF';
+  if (plain) opacity = '00';
+
+  return (
+    <View
+      style={[
+        styles.icon,
+        { backgroundColor: color + opacity },
+        style,
+        small ? styles.iconSmall : void 0,
+      ]}
+    >
+      {icon}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  icon: {
+    padding: 10,
+    borderRadius: 14,
+    borderCurve: 'continuous',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  iconSmall: {
+    padding: 6,
+  },
+});
+
+export default PapillonIcon;
