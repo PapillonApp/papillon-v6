c5ecea6495cbfef3e6a6028881028b5ad532d3b2
LeGeek01 <timeobailly02@gmail.com>
2024-01-23 10:31:59 +0100

    rajout détection crash + message

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+
 registerRootComponent(App);
 
 import * as Sentry from '@sentry/react-native';
+
+import { setJSExceptionHandler } from "react-native-exception-handler";
+
+import { Alert } from 'react-native';
+import RNRestart from 'react-native-restart';
+
+setJSExceptionHandler((error, isFatal) => {
+  Sentry.captureException(error)
+  console.error("Error caught! ", error, isFatal)
+  Alert.alert(
+    "Une erreur est survenue",
+    "Cette erreur nous a été envoyée. Nous vous recommandons de redémarrer l'appli, mais vous pouvez aussi tenter de continuer son fonctionnement." + `\n${error}`,
+    [{
+      "text": "Redémarrer",
+      onPress: () => { RNRestart.Restart(); }
+    },
+    {
+      "text": "Continuer"
+    }]
+  )
+}, true);
 
 Sentry.init({
     dsn: 'http://4f5fa3f4dc364796bbdac55029146458@sentry.getpapillon.xyz/3',
