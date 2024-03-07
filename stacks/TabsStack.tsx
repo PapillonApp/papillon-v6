b95705a396a447957be36d431fcb3a173d49897b
LeGeek01 <54872374+LeGeek01@users.noreply.github.com>
2024-03-07 19:28:05 +0100

    restore old nav bar

--- stacks/TabsStack.tsx
+++ stacks/TabsStack.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { View, Platform, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
+import { BottomNavigation, useTheme } from 'react-native-paper';
+import { PressableScale } from 'react-native-pressable-scale';
+import { useMemo } from 'react';
 
+const theme = useTheme();
 import {
   Home as PapillonIconsHome,
   HomeFill as PapillonIconsHomeFill,
@@ -84,7 +88,7 @@ const TabsStack = ({ navigation }) => {
     hideTabBarTitle: true,
   };
 
-  return (
+  /*return (
     <View style={{ flex: 1 }}>
       <Tab.Navigator
         screenOptions={{
@@ -125,7 +129,118 @@ const TabsStack = ({ navigation }) => {
         ))}
       </Tab.Navigator>
     </View>
+  );*/
+  const tabBar = useMemo(() => {
+    if (Platform.OS !== 'ios') {
+      return ({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          compact={false}
+          shifting={false}
+          safeAreaInsets={{
+            ...insets,
+            right: 12,
+            left: 12,
+          }}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!event.defaultPrevented) {
+              navigation.navigate(route.name);
+            } else {
+              preventDefault();
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                  ? options.title
+                  : route.title;
+
+            return label;
+          }}
+        />
+      );
+    }
+    return undefined;
+  }, []);
+  return (
+    <Tab.Navigator
+      tabBar={tabBar}
+      screenOptions={{
+        headerTruncatedBackTitle: 'Retour',
+        elevated: false,
+        tabBarLabelStyle: {
+          fontFamily: 'Papillon-Medium',
+          fontSize: 12.5,
+          letterSpacing: 0.2,
+
+          margin: 0,
+          padding: 0,
+
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+        },
+        headerTitleStyle: {
+          fontFamily: 'Papillon-Semibold',
+        },
+        tabBarShowLabel: settings?.hideTabBarTitle ? false : true,
+        tabBarActiveTintColor:
+          !settings?.hideTabBarTitle ? UIColors.primary :
+            theme.dark ? '#ffffff' : '#000000',
+        tabBarInactiveTintColor: 
+          settings?.hideTabBarTitle ?
+            theme.dark ? '#ffffff' : '#000000' :
+            theme.dark ? '#ffffff77' : '#00000077',
+        tabBarStyle: {
+          paddingHorizontal: 8,
+          backgroundColor: UIColors.background,
+          borderTopWidth: UIColors.dark ? 0 : 0.5,
+        },
+        tabBarButton: settings?.hideTabBarTitle ? (props) => {
+          return (
+            <PressableScale
+              {...props}
+              activeScale={settings?.hideTabBarTitle ? 1.2 : 0.9}
+              weight='light'
+              style={[
+                {
+                  ...props.style,
+                  flex: 1,
+                  opacity: props.accessibilityState.selected ? 1 : 0.5,
+                },
+              ]}
+            />
+          );
+        } : undefined,
+      }}
+    >
+      {views.map((view, index) => (
+          <Tab.Screen
+            key={index}
+            name={view.name}
+            component={view.component}
+            options={view.options}
+          />
+        ))}
+    </Tab.Navigator>
   );
+
 };
 
 export default TabsStack;
