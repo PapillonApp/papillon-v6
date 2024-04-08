6fac455158a693bd1d759aad5b93504c3938b93e
Tom Theret <tomnils.theret@gmail.com>
2024-04-08 21:38:36 +0200

    fix(widget_ios): Affichage détaillé des significants types + EDT

--- ios/PapillonWidget/Grade/View/SystemMediumGradeWidget.swift
+++ ios/PapillonWidget/Grade/View/SystemMediumGradeWidget.swift
@@ -56,6 +56,12 @@ struct SystemMediumGradeWidget: View {
                                                 .font(Font.system(size: 18))
                                         )
                                     VStack(alignment: .leading, spacing: 4) {
+                                      if grade.description.isEmpty {
+                                        Text(grade.subject)
+                                            .font(Font.system(size: 15))
+                                            .bold()
+                                            .lineLimit(1)
+                                      } else {
                                         Text(grade.description)
                                             .font(Font.system(size: 15))
                                             .bold()
@@ -64,16 +70,50 @@ struct SystemMediumGradeWidget: View {
                                             .lineLimit(1)
                                             .font(Font.system(size: 12))
                                             .foregroundStyle(Color.primary.opacity(0.5))
+                                      }
+
                                     }
                                     Spacer()
                                     VStack(alignment: .trailing, spacing: 0) {
+                                      if grade.grade.value.significant == true {
+                                        switch grade.grade.value.value {
+                                          case 1 : Text("Abs")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 2 : Text("Disp.")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 3 : Text("N.not")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 4 : Text("Inap.")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 5 : Text("N.Rdu")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 6 : Text("Abs.0")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 7 : Text("N.Rdu.0")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          case 8 : Text("Félicitation")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                          default: Text("Erreur")
+                                            .font(.system(size: 17, design: .rounded))
+                                            .bold()
+                                        }
+                                      } else {
                                         Text(String(format: "%.2f", grade.grade.value.value))
                                             .font(.system(size: 17, design: .rounded))
                                             .bold()
-                                        Text("/" + String(format: "%.0f", grade.grade.outOf.value))
-                                            .font(.system(size: 13, design: .rounded))
-                                            .baselineOffset(2)
-                                            .foregroundStyle(Color.primary.opacity(0.5))
+                                      }
+                                      Text("/" + String(format: "%.0f", grade.grade.outOf.value))
+                                          .font(.system(size: 13, design: .rounded))
+                                          .baselineOffset(2)
+                                          .foregroundStyle(Color.primary.opacity(0.5))
                                     }
                                 }
                             }
